546533524ee8584000b6e14243fa6721183add01
unknown <mattcmej@gmail.com>
2018-04-30 14:29:30 -0700

    Finish longest string algorithm.

--- Find-Longest-String/findLongestWord.js
+++ Find-Longest-String/findLongestWord.js
@@ -10,20 +10,39 @@ Steps:
 3. The function will find the length of the longest word and return that number.
 */
 
-
+var test1 = "The quick brown fox jumped over the lazy dog";
+var test2 = "May the force be with you";
+var test3 = "Google do a barrel roll";
+var test4 = "What is the average airspeed velocity of an unladen swallow";
+var test5 = "What if we try a super-long word such as otorhinolaryngology";
 
 function findLongestWord(str) {
     
     var wordArray = str.split(" ");
-    console.log(wordArray);
-    var longestWord;
+    // console.log(wordArray);
+    var longestWord = wordArray[0];
 
-    // for (i = 0; i < wordArray.length; i++) {
+    for (i = 0; i < wordArray.length; i++) {
+        if (wordArray[i].length > longestWord.length) {
+            longestWord = wordArray[i];
+        }
 
-    // }
+        else {
+            continue;
+        }
+    }
+    // console.log(longestWord);
+    return longestWord.length;
 
     // return str.length;
   }
   
-  findLongestWord("The quick brown fox jumped over the lazy dog");
+//   findLongestWord("The quick brown fox jumped over the lazy dog");
+
+console.log("Length of sentence 1: " + findLongestWord(test1));
+console.log("Length of sentence 2: " + findLongestWord(test2));
+console.log("Length of sentence 3: " + findLongestWord(test3));
+console.log("Length of sentence 4: " + findLongestWord(test4));
+console.log("Length of sentence 5: " + findLongestWord(test5));
+
   
